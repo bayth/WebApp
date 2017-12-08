6efb9feb7d6f2cd4bb5b0bef16e8b419cf28b929
bayth <joseph.l.mazzola@gmail.com>
2017-12-08 20:30:47 +0100

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Library
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
